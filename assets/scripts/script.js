fbbf4636719d7bee1744971f0fd1e4bf7864d920
Allyson-mkw <allysoneduardo04@gmail.com>
2021-07-06 23:18:58 -0300

    feat: added function to change width and height

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,21 +7,42 @@ inputs.forEach((element) => {
     })
 })
 
-function changeDiv(element) {
-    console.log(element.target.name)
-    console.log(element.target.value + "px")
+function changeDiv(input) {
+    console.log(input.target.name)
+    console.log(input.target.value + "px")
 
-    switch (element.target.name) {
+    switch (input.target.name) {
     case "border-radius":
-        document.getElementById("special-div").style.borderRadius = (element.target.value + "px")
+        document.getElementById("special-div").style.borderRadius = (input.target.value + "px")
     break;
 
     case "border-color":
-        document.getElementById("special-div").style.borderColor = element.target.value
+        document.getElementById("special-div").style.borderColor = input.target.value
+        document.querySelectorAll("input[name=border-color]").forEach((element) => {element.value = input.target.value})
     break;
 
     case "border-width":
-        document.getElementById("special-div").style.borderWidth = (element.target.value + "px")
+        document.getElementById("special-div").style.borderWidth = (input.target.value + "px")
+    break;
+
+    case "width":
+        document.getElementById("special-div").style.width = (input.target.value + "px")
+        inputCheckboxValue = input.target.parentNode.querySelector("small input[type=checkbox]").checked
+        if(inputCheckboxValue) {
+            document.querySelectorAll("input.size").forEach((element) => {element.value = input.target.value})
+        }
+    break;
+
+    case "height":
+        document.getElementById("special-div").style.height = (input.target.value + "px")
+        inputCheckboxValue = input.target.parentNode.querySelector("small input[type=checkbox]").checked
+        if(inputCheckboxValue) {
+            document.querySelectorAll("input.size").forEach((element) => {element.value = input.target.value})
+        }
     break;
     }
+}
+
+function squareDiv() {
+
 }
